7cde5f228dec6e520c6858bfbe0a5d03fae54a19
yrnmama <mamadou.gningue@marconirovereto.it>
2025-02-08 13:45:59 +0100

    prima patre di js

---
+++ app.js
@@ -0,0 +1,8 @@
+let name = "mamadou"; 
+let cognome = "gningue"; 
+let matricola = "ASFREALMAMA"; 
+
+console.log="name"; 
+console.log= "cognome"; 
+console.log="matricola";
+console.log= ('sono ${name} ${cognome} e la mia matricla è  ${matricola}');
