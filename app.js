6cedeb969a25381acb0fde145aa6bc854464802f
Gningue Mamadou <91191658+YRNMAMA@users.noreply.github.com>
2025-02-12 19:29:54 +0000

    prima parte di conversione da Stringa a numb

--- app.js
+++ app.js
@@ -8,3 +8,18 @@ console.log="name";
 console.log= "cognome"; 
 console.log="matricola";
 console.log= ('sono ${name} ${cognome} e la mia matricla è  ${matricola}');
+
+//Parte due degli esercizi 
+
+let matricolaNum = 2383123; 
+// crea
+let matricolaString= matricolaNum.toString();
+console.log=matricolaString;
+let matricolaRiconvertita= Number(matricolaString);
+console.log= (typeof matricolaString);
+
+
+
+// per Converitre un numero in stringa si utilizza il metodo toString() e 
+// per convertire una stringa in numero si utilizza il metodo Number().
+// con il typeof si può verificare il tipo di dato di una variabile.
