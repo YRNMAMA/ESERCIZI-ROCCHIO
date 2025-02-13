ea309927be35ecfeceee94c06e4bcc61fc14a121
yrnmama <mamadou.gningue@marconirovereto.it>
2025-02-13 18:15:36 +0100

    parte 4

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ console.log="name";
 console.log= "cognome"; 
 console.log="matricola";
 console.log= ('sono ${name} ${cognome} e la mia matricla è  ${matricola}');
+
+
+
+let punteggio = 0; 
+punteggio += 10; // Aggiunge 10
+punteggio -= 3;  // Sottrae 3
+punteggio *= 2;  // Moltiplica per 2
+punteggio /= 4;  // Divide per 4
+punteggio %= 3;  // Restituisce il resto della divisione per 3
+punteggio **= 2; // Eleva al quadrato
+
+console.log(punteggio); 
